84566f4db4818c2408b7ab160747c92e71880fff
Erwann Gache <egache@z1r9p4.42lyon.fr>
2025-12-10 21:59:47 +0100

    created repo

---
+++ ex00/includes/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egache <egache@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 13:16:14 by egache            #+#    #+#             */
+/*   Updated: 2025/12/09 13:22:50 by egache           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <iostream>
+
+class Zombie
+{
+	public:
+		Zombie(void);
+		~Zombie(void);
+
+		void announce(void);
+		Zombie* newZombie(std::string name);
+		void randomChump(std::string name);
+
+	private:
+		std::string _name;
+};
+
+#endif
---
+++ ex00/src/Zombie.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egache <egache@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 13:18:57 by egache            #+#    #+#             */
+/*   Updated: 2025/12/09 13:23:45 by egache           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(void)
+{
+	std::cout << "Zombie constructor appelé" << std::endl;
+	return ;
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << "Zombie destructor appelé" << std::endl;
+	return ;
+}
+
+void Zombie::announce(void)
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ...";
+}
+
+Zombie* Zombie::newZombie(std::string name)
+{
+
+}
+
+void Zombie::randomChump(std::string name)
+{
+	
+}
