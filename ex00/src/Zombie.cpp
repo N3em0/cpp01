a8c5b7088622acdfb9e77957adac854036c4c117
teatimefromhome <teatime@teatime.yo>
2025-12-12 00:44:26 +0100

    implemented some functions | doesn't work atm

--- ex00/src/Zombie.cpp
+++ ex00/src/Zombie.cpp
@@ -24,11 +24,6 @@ Zombie::~Zombie(void)
 	return ;
 }
 
-Zombie::Zombie(std::string name)
-{
-  _name = name;
-}
-
 void Zombie::announce(void)
 {
 	std::cout << this->_name << ": BraiiiiiiinnnzzzZ...";
